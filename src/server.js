3879af2a9d6df242f38a481e245a52d3adb1ec83
alfian <alfiansakuragi@gmail.com>
2026-08-17 10:00:44 +0800

    new: 1.0.2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const app = require('./app.js')
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`server starting on port: ${PORT}`)
